8549d953e71e684c396a0695ea80a183c52f6762
Eduardo Damasceno <damasceno.eduardo85@gmail.com>
2024-07-23 20:34:24 -0300

    setup pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ensureStartsWith } from 'lib/utils';
 import { ReactNode } from 'react';
 import './globals.css';
 
+import type { Metadata, Viewport } from 'next';
+
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
@@ -11,11 +13,15 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
 const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
 const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
-export const metadata = {
+const APP_DEFAULT_TITLE = 'My Awesome PWA App';
+const APP_TITLE_TEMPLATE = `%s | ${SITE_NAME}`;
+const APP_DESCRIPTION = 'Best PWA app in the world!';
+
+export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
     default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`
+    template: APP_TITLE_TEMPLATE
   },
   robots: {
     follow: true,
@@ -28,7 +34,40 @@ export const metadata = {
         creator: twitterCreator,
         site: twitterSite
       }
-    })
+    }),
+  applicationName: SITE_NAME,
+  description: APP_DESCRIPTION,
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: 'website',
+    siteName: SITE_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF'
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
